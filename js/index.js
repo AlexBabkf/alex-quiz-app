a97a8b0efa33c0304fa21b4d32238873d7e55bb4
Georgios Alexandros Christodoulou <bab_alt@hotmail.com>
2023-05-02 11:50:00 +0200

    show/hide answer

--- js/index.js
+++ js/index.js
@@ -1,8 +1,26 @@
-const revealAnswer = document.querySelectorAll('[data-js="reveal-answer"]');
+// Reveal / Hide Answer
 
-revealAnswer.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const answer = btn.nextElementSibling;
-    answer.toggleAttribute("hidden");
-  });
+const revealAnswer = document.querySelector('[data-js="reveal-answer"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+revealAnswer.addEventListener("click", () => {
+  if (answer.innerHTML) {
+    answer.innerHTML = "";
+    revealAnswer.value = "Show Answer";
+  } else {
+    answer.innerHTML = "This is the answer";
+    revealAnswer.innerHTML = "Hide Answer";
+  }
+});
+
+// Change Bookmark Icon
+
+const icon = document.querySelector('[data-js="icon"]');
+
+icon.addEventListener("click", () => {
+  if (icon.src.match("assets/bookmark.png")) {
+    icon.src = "assets/bookmark_filled.png";
+  } else {
+    icon.src = "assets/bookmark.png";
+  }
 });
